e406e3162cf5851fbf13f67a5266918b1d2d88b6
Quang Le <lhiu80@gmail.com>
2019-12-20 17:51:04 +0700

    edit templates

--- Booking_Hotel_QNT/hotel/urls.py
+++ Booking_Hotel_QNT/hotel/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
-from .views import formview
+from .views import indexView,galleryView,asView,bookingView,failedView,blogView
+
+app_name="hotel"
+
 urlpatterns = [
-   path('', formview.as_view(),name="index"),
+   path('', indexView.as_view(),name="index"),
+   path('gallery/', galleryView.as_view(), name="gallery"),
+   path('as/', asView.as_view(), name="as"),
+   path('booking/', bookingView.as_view(), name="booking"),
+   path('404page/', failedView.as_view(), name="404"),
+   path('blog/', blogView.as_view(), name="blog"),
 ]
--- Booking_Hotel_QNT/hotel/views.py
+++ Booking_Hotel_QNT/hotel/views.py
@@ -2,6 +2,26 @@ from django.shortcuts import render
 from django.views import View
 # Create your views here.
 
-class formview(View):
+class indexView(View):
     def get(self,request):
-        return render(request, 'hoteltp/index-2.html')
+        return render(request, 'hoteltp/index.html')
+    
+class galleryView(View):
+    def get(self,request):
+        return render(request, 'hoteltp/gallery.html')
+    
+class asView(View):
+    def get(self,request):
+        return render(request, 'hoteltp/about-us.html')
+    
+class bookingView(View):
+    def get(self,request):
+        return render(request, 'hoteltp/booking.html')
+
+class failedView(View):
+    def get(self,request):
+        return render(request, 'hoteltp/404.html')
+    
+class blogView(View):
+    def get(self,request):
+        return render(request, 'hoteltp/blog.html')    
