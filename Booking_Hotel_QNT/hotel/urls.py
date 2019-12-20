5bd05c27f99016978a4a89676f7c09043cf2dc22
Quang Le <lhiu80@gmail.com>
2019-12-20 12:19:05 +0700

    edit templates

--- Booking_Hotel_QNT/hotel/urls.py
+++ Booking_Hotel_QNT/hotel/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from .views import formview
 urlpatterns = [
-   path('', formview.as_view(),name="test"),
+   path('', formview.as_view(),name="index"),
 ]
--- Booking_Hotel_QNT/hotel/views.py
+++ Booking_Hotel_QNT/hotel/views.py
@@ -1,12 +1,7 @@
 from django.shortcuts import render
 from django.views import View
-from django.http.response import HttpResponse
-from .forms import formPhong
-from phong.models import Phong
 # Create your views here.
 
 class formview(View):
     def get(self,request):
-        form= Phong.objects.all()
-        context={"f":form}
-        return render(request, 'hoteltp/test.html',context)
+        return render(request, 'hoteltp/index.html')
