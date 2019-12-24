608f21b7fff2cf89a0f7c6fafb076633f4e04fad
Quang Le <lhiu80@gmail.com>
2019-12-24 18:00:18 +0700

    update

--- Booking_Hotel_QNT/chitietbooking/models.py
+++ Booking_Hotel_QNT/chitietbooking/models.py
@@ -4,11 +4,9 @@ from phong.models import Phong
 from django.utils import timezone
 # Create your models here.
 class ChiTietBooking(models.Model):
-    mabooking= models.CharField(max_length=30,primary_key=True)
+    mabooking= models.AutoField(primary_key=True)
     tendn= models.ForeignKey(MyUser, on_delete=models.CASCADE)
     sophong= models.ForeignKey(Phong, on_delete=models.CASCADE,default=0)
     ngaynhanphong= models.DateTimeField(default= timezone.now)
     ngaytratphong= models.DateTimeField(default= timezone.now)
     thanhtien=models.BigIntegerField(default=0)
-    def __str__(self):
-        return self.mabooking
